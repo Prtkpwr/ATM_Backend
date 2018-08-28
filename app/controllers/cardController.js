01431a81feff2755c58f9df19caffc886134629e
pratik <pratik.pawar@vivaconnect.co>
2018-08-28 18:36:55 +0530

    minor

--- app/controllers/cardController.js
+++ app/controllers/cardController.js
@@ -75,43 +75,48 @@ let withdrawMoney = (req, res) => {
             if (result[0].balance >= req.body.amount) {
                 let value = req.body.amount;
                 console.log(req.body.amount)
-                let remainder = value % 100;
-                if (remainder == 0) {
-                    while (value > 0) {
-                        if ((value / 2000) >= 1) {
-                            data.counttwo += Math.floor(value / 2000)
-                            value = value - Math.floor(value / 2000) * 2000
+                if(req.body.amount >=100 ){
+                    let remainder = value % 100;
+                    if (remainder == 0) {
+                        while (value > 0) {
+                            if ((value / 2000) >= 1) {
+                                data.counttwo += Math.floor(value / 2000)
+                                value = value - Math.floor(value / 2000) * 2000
+                            }
+                            else if ((value / 500) >= 1) {
+                                data.countfive += Math.floor(value / 500)
+                                value = value - Math.floor(value / 500) * 500
+                            }
+                            else if ((value / 100) >= 1) {
+                                data.countone += Math.floor(value / 100)
+                                value = value - Math.floor(value / 100) * 100
+                            }
                         }
-                        else if ((value / 500) >= 1) {
-                            data.countfive += Math.floor(value / 500)
-                            value = value - Math.floor(value / 500) * 500
+                        data.new_balance = result[0].balance - req.body.amount
+                        let update = {
+                            balance: data.new_balance
                         }
-                        else if ((value / 100) >= 1) {
-                            data.countone += Math.floor(value / 100)
-                            value = value - Math.floor(value / 100) * 100
-                        }
+                        CardsModel.findOneAndUpdate({
+                            "card_number": req.body.card_number
+                        }, update, function (err, result) {
+    
+                            if (err) {
+                                console.log(err)
+                            } else {
+                                console.log("done")
+                            }
+    
+                        });
                     }
-                    data.new_balance = result[0].balance - req.body.amount
-                    let update = {
-                        balance: data.new_balance
+                    else {
+                        data.message = "Please Enter Amount Multiple of 100"
                     }
-                    CardsModel.findOneAndUpdate({
-                        "card_number": req.body.card_number
-                    }, update, function (err, result) {
-
-                        if (err) {
-                            console.log(err)
-                        } else {
-                            console.log("done")
-                        }
-
-                    });
                 }
-                else {
-                    data.message = "Please Enter Amount Multiple of 100"
+                else{
+                    res.send({"message":"Please Enter Amount Multiple of 100"})
                 }
 
-                res.send(data);
+                res.send({"status":200, data:data});
             }
             else {
                 res.send({
